fafab2e6279c7146fa095a8bef9792ce6d37e305
belhaddadmohamed <belhaddad.mohamed7@gmail.com>
2025-03-02 22:40:25 +0100

    Update images

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Card from "./Components/Card";
+import Image from "next/image";
 
 export default function Home() {
 
@@ -8,9 +9,11 @@ export default function Home() {
       
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
 
-          <h1>BELHADDAD MOHAMED ISLEM IS THE BEST IN THE WORLD OF DATA</h1>
-          <Link href="/users"> Users  </Link>
-          <Card />
+        <h1>BELHADDAD MOHAMED ISLEM IS THE BEST IN THE WORLD OF DATA</h1>
+        <Link href="/users"> Users  </Link>
+        <Image src="https://cdn.staticneo.com/w/naruto/Nprofile2.jpg" alt="Vercel Logo" width={150} height={200} priority />
+        <Card />
+        
       </main>
   );
 }
---
+++ app/users/profile/[id]/page.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+interface Profile {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  website: string;
+}
+
+interface singleProfileProps {
+  params: {id: string}
+}
+
+const SingleProfile = async ({params} : singleProfileProps) => {
+  const  { id } = await params;
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+  const profile: Profile = await res.json();
+
+  if (!res) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return (
+    <div>
+      <h1>{profile.name}</h1>
+      <p>Email: {profile.email}</p>
+      <p>Phone: {profile.phone}</p>
+      <p>Website: {profile.website}</p>
+    </div>
+  );
+};
+
+export default SingleProfile;
