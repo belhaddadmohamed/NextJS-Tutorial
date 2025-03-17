751f1421d8fb4fccb6b08fb070f968b00ecd66d4
belhaddadmohamed <belhaddad.mohamed7@gmail.com>
2025-03-17 18:32:24 +0100

    Update validation messages

--- utils/validationSchema.ts
+++ utils/validationSchema.ts
@@ -1,9 +1,16 @@
-import { z } from 'zod'
-
+import { z } from "zod";
 
 export const createArticleSchema = z.object({
-    title: z.string().
-             min(2, "Title must be more than 2 characters").
-             max(200),
-    description: z.string().min(10).max(500, "Please less than 500 chars in description"),
-})
+  title: z
+    .string({
+      required_error: "Title is required",
+      invalid_type_error: "Title is Should be String",
+    })
+    .min(2, {message: "Title must be more than 2 characters"})
+    .max(200),
+
+  description: z
+    .string()
+    .min(10)
+    .max(500, "Please less than 500 chars in description"),
+});
